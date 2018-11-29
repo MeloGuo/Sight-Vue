3781f83d21da73df49edcb075c58f4b400a78e38
MeloGuo <guoziliang199606@gmail.com>
2018-11-29 18:07:20 +0800

    feat: 加入parcel，将button改为单文件组件

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import Button from './button'
+
+Vue.component('s-button', Button)
+
+new Vue({
+  el: '#app'
+})
--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,32 @@
 <template>
-  $END$
+  <button class="s-button">按钮</button>
 </template>
 
 <script>
   export default {
-    name: 'button'
+
   }
 </script>
 
-<style scoped>
+<style lang="scss">
+  .s-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
 
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+
+    &:active {
+      background-color: var(--button-active-bg);
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
 </style>
