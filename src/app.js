c910ba99eb07c2f54edbe1f01d883cd7b4af4b3c
MeloGuo <guoziliang199606@gmail.com>
2018-11-29 21:34:44 +0800

    feat(s-button-group): 创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('s-button', Button)
 Vue.component('s-icon', Icon)
+Vue.component('s-button-group', ButtonGroup)
 
 const vm = new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="s-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style lang="scss">
+  .s-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .s-button {
+      border-radius: 0;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+        margin-right: -1px;
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+        margin-left: -1px;
+      }
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+</style>
