08dec468d3b389f0990e74e683f3fcf281cc48be
MeloGuo <guoziliang199606@gmail.com>
2018-12-03 16:52:22 +0800

    feat(s-input): 添加 input.vue 及其样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
+import Input from './input'
 
 Vue.component('s-button', Button)
 Vue.component('s-icon', Icon)
 Vue.component('s-button-group', ButtonGroup)
+Vue.component('s-input', Input)
 
 const vm = new Vue({
   el: '#app',
