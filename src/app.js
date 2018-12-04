790be7eb82ac2df6fd2e80c751040e7b0d813533
MeloGuo <guoziliang199606@gmail.com>
2018-12-04 13:37:28 +0800

    feat(s-row, s-col): 网格系统的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 
 Vue.component('s-button', Button)
 Vue.component('s-icon', Icon)
 Vue.component('s-button-group', ButtonGroup)
 Vue.component('s-input', Input)
+Vue.component('s-col', Col)
+Vue.component('s-row', Row)
 
 const vm = new Vue({
   el: '#app',
---
+++ src/col.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="col" :class="[`col-${span}`]">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SightCol',
+    props: {
+      span: {
+        type: [Number, String],
+        validator (value) {
+          value = parseInt(value)
+          return value > 0 && value <= 24
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  .col {
+    height: 100px;
+    background-color: grey;
+    width: 50%;
+    border: 1px solid red;
+
+    $prefix: col-;
+    @for $n from 1 through 24 {
+      &.#{$prefix}#{$n} {
+        width: ($n / 24) * 100%;
+      }
+    }
+  }
+</style>
