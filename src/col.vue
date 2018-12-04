c60b3df997fc0b8bf8829ad1fd4c74c989bc80f4
MeloGuo <guoziliang199606@gmail.com>
2018-12-04 22:13:49 +0800

    feat(s-col): col 组件增加 xs 组件支持自适应

--- src/col.vue
+++ src/col.vue
@@ -5,6 +5,11 @@
 </template>
 
 <script>
+  const validator = function (value) {
+    let keys = Object.keys(value)
+    return keys.every((key) => ['span', 'offset'].includes(key))
+  }
+
   export default {
     name: 'SightCol',
     props: {
@@ -21,6 +26,10 @@
           value = parseInt(value)
           return value > 0 && value <= 24
         }
+      },
+      xs: {
+        type: Object,
+        validator
       }
     },
     data () {
@@ -30,10 +39,16 @@
     },
     computed: {
       colClasses () {
-        const { span, offset } = this
+        const {span, offset, xs} = this
+        let xsClass = []
+        if (xs) {
+          xsClass = [`col-xs-${xs.span}`, `offset-xs-${xs.offset}`]
+        }
+
         return [
           span && `col-${span}`,
-          offset && `offset-${offset}`
+          offset && `offset-${offset}`,
+          ...xsClass
         ]
       },
       gutterStyle () {
@@ -62,6 +77,22 @@
     @for $n from 1 through 24 {
       &.#{$prefix}#{$n} {
         margin-left: ($n / 24) * 100%;
+      }
+    }
+
+    @media (max-width: 575px) {
+      $prefix: col-xs-;
+      @for $n from 1 through 24 {
+        &.#{$prefix}#{$n} {
+          width: ($n / 24) * 100%;
+        }
+      }
+
+      $prefix: offset-xs-; /* TODO:解决 offset-xs-undefined 的问题 */
+      @for $n from 0 through 24 {
+        &.#{$prefix}#{$n} {
+          margin-left: ($n / 24) * 100%;
+        }
       }
     }
   }
