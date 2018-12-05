01dd3cb84ea7e64bec5c8430b46c02eb40687c86
MeloGuo <guoziliang199606@gmail.com>
2018-12-05 15:42:33 +0800

    feat(s-layout): 实现 layout 基础布局

---
+++ src/layout.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="layout" :class="layoutClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SightLayout',
+    mounted () {
+      this.$children.forEach((child) => {
+        if (child.$options.name === 'SightSider') {
+          this.layoutClass['layout-has-sider'] = true
+        }
+      })
+    },
+    data () {
+      return {
+        layoutClass: {
+          'layout-has-sider': false
+        }
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .layout {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+
+    &.layout-has-sider {
+      flex-direction: row;
+    }
+  }
+</style>
---
+++ src/sider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="sider">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'SightSider'
+  }
+</script>
+
+<style lang="scss" scoped>
+  .sider {
+    position: relative;
+  }
+</style>
