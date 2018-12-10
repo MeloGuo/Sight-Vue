e1520d0185be1de72b569b80ad1585795c78b4e7
MeloGuo <guoziliang199606@gmail.com>
2018-12-10 15:25:21 +0800

    feat(s-tabs): 完善 tabs 组件样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -12,9 +12,12 @@
   export default {
     name: 'SightTabsHead',
     inject: ['eventBus'],
-    created () {
+    mounted () {
       this.eventBus.$on('update:selected', (item, selected) => {
-        console.log(item, selected)
+        this.x = true
+        let { width, height, top, left } = selected.$el.getBoundingClientRect()
+        this.$refs.line.style.width = `${width}px`
+        this.$refs.line.style.left = `${left}px`
       })
     }
   }
@@ -23,24 +26,26 @@
 <style scoped lang="scss">
   $tab-height: 40px;
   $blue: blue;
+  $border-color: #ddd;
   .tabs-head {
     display: flex;
     height: $tab-height;
     justify-content: flex-start;
-    border: 1px solid red;
     position: relative;
+    border-bottom: 1px solid $border-color;
 
     > .line {
-      width: 50px;
       position: absolute;
       bottom: 0;
-      border-bottom: 4px solid $blue;
+      border-bottom: 1px solid $blue;
+      transition: all 250ms;
     }
 
     > .actions-wrapper {
       margin-left: auto;
       display: flex;
       align-items: center;
+      padding: 0 1em;
     }
   }
 </style>
