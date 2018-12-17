b61cb6c6f4e24d6d9039e9b3a446f6c825c50e00
MeloGuo <guoziliang199606@gmail.com>
2018-12-17 22:39:02 +0800

    fix: karma.config.js 忘记修改路径了

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@ const webpackConfig = require('./webpack.config.js')
 module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
+    basePath: __dirname,
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
@@ -11,7 +11,7 @@ module.exports = function (config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'test/**/*.spec.js'
+      'tests/**/*.spec.js'
     ],
 
     // list of files / patterns to exclude
