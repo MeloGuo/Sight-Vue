f91fe18f1475bdce81f1d558dabc10f962dbba1b
MeloGuo <guoziliang199606@gmail.com>
2018-12-18 11:44:25 +0800

    fix: 意外 ignore 了 icon 文件夹导致 ci 无法跑通

---
+++ src/components/icon/icon.vue
@@ -0,0 +1,23 @@
+<template>
+  <svg class="s-icon">
+    <use :xlink:href=`#i-${name}`></use>
+  </svg>
+</template>
+
+<script>
+  import './svg.js'
+
+  export default {
+    name: 'SightIcon',
+    props: {
+      name: String
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .s-icon {
+    width: 1em;
+    height: 1em;
+  }
+</style>
