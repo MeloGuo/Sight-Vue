8ded4b4e80e515f5a94053e8574cb7c420de4880
MeloGuo <guoziliang199606@gmail.com>
2018-12-20 20:34:05 +0800

    feat(s-validator): 组件可以添加自定义 validator
    re #2

--- src/components/validate/validate.js
+++ src/components/validate/validate.js
@@ -1,74 +1,66 @@
-export default function validate (data, rules) {
-  const errors = {}
+export default class Validator {
+  static ensureObject (object, key) {
+    if (!object[key]) {
+      object[key] = {}
+    }
+  }
 
-  rules.forEach((rule) => {
-    const value = data[rule.key]
-    if (rule.required) {
-      const error = validate.required(value)
-      if (error) {
-        ensureObject(errors, rule.key)
-        errors[rule.key].required = error
-        return
+  static add (name, fn) {
+    this[name] = fn
+  }
+
+  validate (data, rules) {
+    const errors = {}
+
+    rules.forEach((rule) => {
+      const value = data[rule.key]
+      if (rule.required) {
+        const error = this.required(value)
+        if (error) {
+          Validator.ensureObject(errors, rule.key)
+          errors[rule.key].required = error
+          return
+        }
       }
+
+      const validators = Object.keys(rule).filter(key => (
+        key !== 'key' && key !== 'required')
+      )
+      validators.forEach((validatorKey) => {
+        if (this[validatorKey]) {
+          const error = this[validatorKey](value, rule[validatorKey])
+          if (error) {
+            Validator.ensureObject(errors, rule.key)
+            errors[rule.key][validatorKey] = error
+          }
+        } else {
+          throw new Error(`Invalid validator of '${validatorKey}'`)
+        }
+      })
+    })
+
+    return errors
+  }
+
+  required (value) {
+    if (!value && value !== 0) {
+      return '必填'
+    }
+  }
+
+  pattern (value, pattern) {
+    if (pattern === 'email') {
+      pattern = /^.+@.+$/
     }
 
-    // if (rule.pattern) {
-    //   const error = validate.pattern(value, rule.pattern)
-    //   if (error) {
-    //     ensureObject(errors, rule.key)
-    //     errors[rule.key].pattern = error
-    //   }
-    // }
-    //
-    // if (rule.minLength) {
-    //   const error = validate.minLength(value, rule.minLength)
-    //   if (error) {
-    //     ensureObject(errors, rule.key)
-    //     errors[rule.key].minLength = error
-    //   }
-    // }
-
-    const validators = Object.keys(rule).filter(key => key !== 'key' && key !== 'required')
-    validators.forEach((validatorKey) => {
-      if (validate[validatorKey]) {
-        const error = validate[validatorKey](value, rule[validatorKey])
-        if (error) {
-          ensureObject(errors, rule.key)
-          errors[rule.key][validatorKey] = error
-        }
-      } else {
-        throw new Error(`Invalid validator of '${validatorKey}'`)
-      }
-    })
-  })
-
-  return errors
-}
-
-validate.required = (value) => {
-  if (!value && value !== 0) {
-    return '必填'
-  }
-}
-
-validate.pattern = (value, pattern) => {
-  if (pattern === 'email') {
-    pattern = /^.+@.+$/
+    if (pattern.test(value) === false) {
+      return '格式不正确'
+    }
   }
 
-  if (pattern.test(value) === false) {
-    return '格式不正确'
-  }
-}
-
-validate.minLength = (value, minLength) => {
-  if (minLength > value.length) {
-    return '太短'
-  }
-}
-
-function ensureObject (object, key) {
-  if (!object[key]) {
-    object[key] = {}
+  minLength (value, minLength) {
+    if (minLength > value.length) {
+      return '太短'
+    }
   }
 }
