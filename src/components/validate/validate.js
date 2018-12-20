7a028d97cee1f07d9e440b24e5c9c9357a3a3c16
MeloGuo <guoziliang199606@gmail.com>
2018-12-20 19:42:42 +0800

    refactor(s-validator): 使用迭代器模式重构

--- src/components/validate/validate.js
+++ src/components/validate/validate.js
@@ -4,34 +4,67 @@ export default function validate (data, rules) {
   rules.forEach((rule) => {
     const value = data[rule.key]
     if (rule.required) {
-      if (!value && value !== 0) {
-        errors[rule.key] = {
-          required: '必填'
-        }
+      const error = validate.required(value)
+      if (error) {
+        ensureObject(errors, rule.key)
+        errors[rule.key].required = error
         return
       }
     }
 
-    if (rule.pattern) {
-      if (rule.pattern === 'email') {
-        rule.pattern = /^.+@.+$/
-      }
+    // if (rule.pattern) {
+    //   const error = validate.pattern(value, rule.pattern)
+    //   if (error) {
+    //     ensureObject(errors, rule.key)
+    //     errors[rule.key].pattern = error
+    //   }
+    // }
+    //
+    // if (rule.minLength) {
+    //   const error = validate.minLength(value, rule.minLength)
+    //   if (error) {
+    //     ensureObject(errors, rule.key)
+    //     errors[rule.key].minLength = error
+    //   }
+    // }
 
-      if (rule.pattern.test(value) === false) {
-        ensureObject(errors, rule.key)
-        errors[rule.key].pattern = '格式不正确'
+    const validators = Object.keys(rule).filter(key => key !== 'key' && key !== 'required')
+    validators.forEach((validatorKey) => {
+      if (validate[validatorKey]) {
+        const error = validate[validatorKey](value, rule[validatorKey])
+        if (error) {
+          ensureObject(errors, rule.key)
+          errors[rule.key][validatorKey] = error
+        }
+      } else {
+        throw new Error(`Invalid validator of '${validatorKey}'`)
       }
-    }
-
-    if (rule.minLength) {
-      if (rule.minLength > value.length) {
-        ensureObject(errors, rule.key)
-        errors[rule.key].minLength = '太短'
-      }
-    }
+    })
   })
 
   return errors
+}
+
+validate.required = (value) => {
+  if (!value && value !== 0) {
+    return '必填'
+  }
+}
+
+validate.pattern = (value, pattern) => {
+  if (pattern === 'email') {
+    pattern = /^.+@.+$/
+  }
+
+  if (pattern.test(value) === false) {
+    return '格式不正确'
+  }
+}
+
+validate.minLength = (value, minLength) => {
+  if (minLength > value.length) {
+    return '太短'
+  }
 }
 
 function ensureObject (object, key) {
