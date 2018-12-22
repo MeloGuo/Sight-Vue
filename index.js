d2cdbe75a97d6059e162b99f54a34dda586a516b
MeloGuo <guoziliang199606@gmail.com>
2018-12-22 09:48:51 +0800

    chore(publish): 增加包入口

---
+++ index.js
@@ -0,0 +1,9 @@
+import Button from './src/components/button/button.vue'
+import ButtonGroup from './src/components/button/button-group.vue'
+import Icon from './src/components/icon/icon.vue'
+
+export {
+  Icon,
+  Button,
+  ButtonGroup
+}
