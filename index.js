82df173500a905ece739782d86f30af2297be164
MeloGuo <guoziliang199606@gmail.com>
2018-12-22 15:02:21 +0800

    build: 修改 build 方法

--- index.js
+++
@@ -1,9 +0,0 @@
-import Button from './src/components/button/button.vue'
-import ButtonGroup from './src/components/button/button-group.vue'
-import Icon from './src/components/icon/icon.vue'
-
-export {
-  Icon,
-  Button,
-  ButtonGroup
-}
