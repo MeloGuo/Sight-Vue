0a8283195a2ce4f959bfce50f1ff6a04412a78d3
MeloGuo <guoziliang199606@gmail.com>
2018-12-24 16:28:04 +0800

    test(s-button): 完善单元测试

--- tests/unit/button.spec.js
+++ tests/unit/button.spec.js
@@ -1,17 +1,73 @@
-import { expect } from 'chai'
+import chai, { expect } from 'chai'
+import sinon from 'sinon'
+import sinonChai from 'sinon-chai'
+import { shallowMount, mount } from '@vue/test-utils'
 import Vue from 'vue'
 import Icon from '../../src/components/icon/icon.vue'
 import Button from '../../src/components/button/button.vue'
 import ButtonGroup from '../../src/components/button/button-group.vue'
 
+chai.use(sinonChai)
+
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
-/* eslint-disable no-unused-expressions */
+/* eslint-disable no-unused-expressions no-undef */
 describe('Button', () => {
-  it('should exist', function () {
+  it('存在', function () {
     [Icon, Button, ButtonGroup].forEach((component) => {
-      expect(component).to.be.exist
+      expect(component).to.exist
     })
+  })
+
+  describe('Icon', function () {
+    it('icon 接受 name 属性', function () {
+      const wrapper = mount(Button, {
+        propsData: {
+          icon: 'message'
+        }
+      })
+      const useElement = wrapper.find('use')
+      expect(useElement.attributes()['href']).to.eq('#i-message')
+    })
+  })
+
+  it('设置 iconPosition 为 right', function () {
+    const wrapper = mount(Button, {
+      attachToDocument: true,
+      propsData: {
+        icon: 'settings',
+        iconPosition: 'right'
+      }
+    })
+    const vm = wrapper.vm
+    const icon = vm.$el.querySelector('svg')
+    expect(getComputedStyle(icon).order).to.eq('2')
+  })
+
+  it('点击 button 触发 click 事件', function () {
+    const wrapper = mount(Button, {
+      propsData: {
+        icon: 'message'
+      }
+    })
+    const vm = wrapper.vm
+    const callback = sinon.fake()
+    vm.$on('click', callback)
+    wrapper.trigger('click')
+    expect(callback).to.have.been.called
+  })
+
+  it('loading 状态下不可点击', function () {
+    const wrapper = mount(Button, {
+      propsData: {
+        loading: true
+      }
+    })
+    const vm = wrapper.vm
+    const callback = sinon.fake()
+    vm.$on('click', callback)
+    wrapper.trigger('click')
+    expect(callback).to.not.have.been.called
   })
 })
