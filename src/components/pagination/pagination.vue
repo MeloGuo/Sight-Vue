f10cd27ad07266282955dee289027f303246de59
MeloGuo <guoziliang199606@gmail.com>
2019-02-02 11:08:22 +0800

    refactor(s-pagination): 重构 pagers 的计算方式

--- src/components/pagination/pagination.vue
+++ src/components/pagination/pagination.vue
@@ -44,6 +44,12 @@
         default: true
       }
     },
+    data () {
+      return {
+        showPrevMore: false,
+        showNextMore: false
+      }
+    },
     computed: {
       pages () {
         const unique = (array) => {
@@ -100,6 +106,45 @@
             array[index + 1] !== undefined && array[index + 1] - current > 1 && prev.push('...')
             return prev
           }, [])
+      },
+      pagers () {
+        const pagerCount = 7;
+        const halfPagerCount = (pagerCount - 1) / 2;
+        const currentPage = Number(this.currentPage);
+        const pageCount = Number(this.totalPage);
+        let showPrevMore = false;
+        let showNextMore = false;
+        if (pageCount > pagerCount) {
+          if (currentPage > pagerCount - halfPagerCount) {
+            showPrevMore = true;
+          }
+          if (currentPage < pageCount - halfPagerCount) {
+            showNextMore = true;
+          }
+        }
+        const array = [];
+        if (showPrevMore && !showNextMore) {
+          const startPage = pageCount - (pagerCount - 2);
+          for (let i = startPage; i < pageCount; i++) {
+            array.push(i);
+          }
+        } else if (!showPrevMore && showNextMore) {
+          for (let i = 2; i < pagerCount; i++) {
+            array.push(i);
+          }
+        } else if (showPrevMore && showNextMore) {
+          const offset = Math.floor(pagerCount / 2) - 1;
+          for (let i = currentPage - offset ; i <= currentPage + offset; i++) {
+            array.push(i);
+          }
+        } else {
+          for (let i = 2; i < pageCount; i++) {
+            array.push(i);
+          }
+        }
+        this.showPrevMore = showPrevMore;
+        this.showNextMore = showNextMore;
+        return array;
       },
       visible () {
         return !(this.hideIfOnePage && this.totalPage === 1)
