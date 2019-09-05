de9966b7d6f86d77982edc437cce2c068c559ba6
MeloGuo <guoziliang199606@gmail.com>
2019-09-05 15:07:46 +0800

    refactor(s-button): adapt the change of theme

---
+++ packages/button/src/button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    class="s-button"
+    @click="handleClick"
+    :class="[
+      `icon-${this.iconPosition}`,
+      {
+        'is-disabled': this.disabled
+      }
+    ]"
+  >
+    <span v-if="$slots.default"><slot></slot></span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SightButton',
+  props: {
+    icon: {
+      type: String,
+      default: ''
+    },
+    iconPosition: {
+      type: String,
+      default: 'left',
+      validator (direction) {
+        return ['left', 'right'].indexOf(direction) > -1
+      }
+    },
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  methods: {
+    handleClick (event) {
+      if (!this.loading && !this.disabled) {
+        this.$emit('click', event)
+      }
+    }
+  }
+}
+</script>
